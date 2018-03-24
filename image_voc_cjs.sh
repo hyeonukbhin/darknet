4ebf0bb449919decc3209e1f4558080d2d259d62
JongSuk Choi <cjs0818@ust.ac.kr>
2018-03-24 11:07:09 +0900

    The first train_test

---
+++ image_voc_cjs.sh
@@ -0,0 +1,6 @@
+
+
+./darknet detector test ./data/data_cjs/obj.data ./cfg/yolo-voc_cjs.cfg ./yolo-voc_cjs_final.weights data/data_cjs/img/air3.jpg
+
+
+
---
+++ train_cjs.sh
@@ -0,0 +1,2 @@
+
+./darknet detector train ./data/data_cjs/obj.data ./cfg/yolo-voc_cjs.cfg ./cfg/darknet19_448.conv.23
