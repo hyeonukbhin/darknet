c3efd163075b9aa2807f4d89cae1b89c632e99d9
cjs0818 <cjs0818@ust.ac.kr>
2018-03-25 11:00:09 +0900

    text_gen.sh added

---
+++ text_gen.sh
@@ -0,0 +1,13 @@
+./darknet rnn generate cfg/rnn.cfg grrm.weights -srand 0 -seed JON
+
+#./darknet detector test ./cfg/voc.data ./cfg/tiny-yolo.cfg ./tiny-yolo.weights data/data_cjs/img/air4.jpg
+
+#./darknet detector test ./cfg/voc.data ./cfg/tiny-yolo.cfg ./tiny-yolo.weights data/data_cjs/img/air1.jpg -i 0 -thresh 0.01
+
+#./darknet detector test ./cfg/voc.data ./cfg/tiny-yolo.cfg ./tiny-yolo.weights data/dog.jpg  -thresh 0.2
+
+#------------------------
+#./darknet detector test ./cfg/voc.data ./cfg/yolo-voc.cfg ./yolo-voc.weights data/dog.jpg -i 0 -thresh 0.2
+#------------------------
+
+
