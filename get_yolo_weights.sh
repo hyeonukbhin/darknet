ba5c84807031a77dc92cb4e8988e3cac3b236400
cjs0818 <cjs0818@ust.ac.kr>
2018-03-25 10:58:07 +0900

    get_yolo_weights.sh removed

--- get_yolo_weights.sh
+++
@@ -1,4 +0,0 @@
-wget https://pjreddie.com/media/files/yolo.weights
-wget https://pjreddie.com/media/files/yolo-voc.weights
-wget https://pjreddie.com/media/files/tiny-yolo.weights
-wget https://pjreddie.com/media/files/yolo9000.weights
