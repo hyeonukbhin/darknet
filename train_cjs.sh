d7e347ed598e4b2e0ea2a8e449ae628ec9b2a2dc
JongSuk Choi <cjs0818@ust.ac.kr>
2018-03-27 16:20:06 +0900

    train_cjs.sh modified

--- train_cjs.sh
+++ train_cjs.sh
@@ -1,2 +1,2 @@
 
-./darknet detector train ./data_cjs/obj.data ./data_cjs/yolo-obj.cfg ~/work/darknet_weights_data/darknet19_448.conv.23
+./darknet detector train ./data_cjs/obj.data ./data_cjs/yolo-obj.cfg ./darknet19_448.conv.23
