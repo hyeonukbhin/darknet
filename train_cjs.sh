17aebf00eb78d7064eb0f0587486c2a2d8b04962
hyeonukbhin <bhu@kist.re.kr>
2018-04-11 09:51:00 +0900

    project complete

--- train_cjs.sh
+++ train_cjs.sh
@@ -1,2 +1,1 @@
-
-./darknet detector train ./data_cjs/obj.data ./data_cjs/yolo-obj.cfg ./darknet19_448.conv.23
+./darknet detector train ./data_cjs/obj.data ./data_cjs/yolo-obj.cfg /home/kist/coursework/darknet_weights_data/darknet19_448.conv.23
---
+++ train_cozmo.sh
@@ -0,0 +1,1 @@
+./darknet detector train ./data_cozmo/obj.data ./data_cozmo/yolo-obj.cfg /home/kist/coursework/darknet_weights_data/darknet19_448.conv.23
